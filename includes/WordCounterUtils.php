31677d45d462b4b89d051a6777644b200414a4d7
komed3 <pk97@outlook.de>
2025-08-17 17:06:29 +0200

    Update WordCounterUtils.php

--- includes/WordCounterUtils.php
+++ includes/WordCounterUtils.php
@@ -1,5 +1,15 @@
 <?php
 
+    /**
+     * Class WordCounterUtils
+     * 
+     * Utility class for WordCounter extension.
+     * 
+     * @author Paul Köhler (komed3)
+     * @license MIT
+     * @since 0.1.0
+     */
+
     namespace MediaWiki\Extension\WordCounter;
 
     use MediaWiki\MediaWikiServices;
@@ -8,8 +18,19 @@
     use MediaWiki\Revision\SlotRecord;
     use MediaWiki\User\User;
 
+    /**
+     * Class WordCounterUtils
+     * 
+     * Utility class for WordCounter extension.
+     */
     class WordCounterUtils {
 
+        /**
+         * Count words from a revision record.
+         *
+         * @param RevisionRecord $revisionRecord - The revision record to count words from
+         * @return int|null - The word count or null if not applicable
+         */
         public static function countWordsFromRevision (
             RevisionRecord $revisionRecord
         ) : ?int {
