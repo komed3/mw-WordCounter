9293ece8b2b81707dd09c79a76dfaabd5af5618c
komed3 <pk97@outlook.de>
2025-08-20 14:26:56 +0200

    improvements + PHPDoc

--- includes/tasks/PurgeOrphaned.php
+++ includes/tasks/PurgeOrphaned.php
@@ -1,25 +1,53 @@
 <?php
 
+    /**
+     * Class PurgeOrphaned
+     * 
+     * This task purges orphaned or invalid word counter entries
+     * from the database. It extends the Task class and implements
+     * the runTask method to perform the cleanup operation.
+     * 
+     * @author Paul Köhler (komed3)
+     * @license MIT
+     * @since 0.1.0
+     */
+
     namespace MediaWiki\Extension\WordCounter\Tasks;
 
     use MediaWiki\Extension\WordCounter\Database;
     use MediaWiki\Extension\WordCounter\Utils;
 
+    /**
+     * Class PurgeOrphaned
+     * 
+     * Implements the task to purge orphaned or invalid word counter entries.
+     */
     class PurgeOrphaned extends Task {
 
+        /**
+         * Runs the task to purge orphaned or invalid word counter entries.
+         * 
+         * @param array $options - Options for the task, including limit and dry-run mode.
+         * @return array - Result of the task execution.
+         */
         public function runTask (
             array $options
-        ) {
+        ) : array {
 
-            $this->output( 'Starting orphaned wordcounter entry cleanup ...' );
+            $this->output( 'Starting orphaned wordcounter entry cleanup.' );
 
             $limit = (int) $options[ 'limit' ] ?: 1000;
             $dryRun = (bool) $options[ 'dry-run' ];
 
+            // Delete orphaned or invalid entries
             $deleted = Database::deleteOrphanedEntries( $limit, $dryRun );
 
-            $this->output( 'Deleted entries: ' . $deleted );
+            $this->output(
+                ( $dryRun ? 'Would delete ' : 'Deleted ' ) .
+                $deleted . ' orphaned entries.'
+            );
 
+            // Clear cache if entries were deleted and not in dry-run mode
             if ( $deleted && ! $dryRun ) {
 
                 Utils::clearCache();
@@ -28,6 +56,12 @@
             }
 
             $this->output( 'Cleanup orphaned wordcounter entry finished.' );
+
+            return [
+                'result' => [
+                    'deleted' => $deleted
+                ]
+            ];
 
         }
 
--- includes/tasks/Task.php
+++ includes/tasks/Task.php
@@ -1,11 +1,43 @@
 <?php
 
+    /**
+     * Class Task
+     * 
+     * Base class for tasks in the WordCounter extension.
+     * Provides methods for outputting messages and setting an output callback.
+     * 
+     * @author Paul Köhler (komed3)
+     * @license MIT
+     * @since 0.1.0
+     */
+
     namespace MediaWiki\Extension\WordCounter\Tasks;
 
+    /**
+     * Class Task
+     * 
+     * This class serves as a base for all tasks in the WordCounter extension.
+     */
     class Task {
 
+        /**
+         * Output callback function.
+         * 
+         * This function is called to output messages during task execution.
+         * It can be set to a custom function that handles the output.
+         * 
+         * @var callable|null
+         */
         private $outputCallback = null;
 
+        /**
+         * Outputs a message.
+         * 
+         * This method checks if an output callback is set and calls it with the provided message.
+         * If no callback is set, it does nothing.
+         * 
+         * @param string $msg - The message to output.
+         */
         protected function output (
             string $msg
         ) : void {
@@ -18,6 +50,13 @@
 
         }
 
+        /**
+         * Sets the output callback function.
+         * 
+         * This method allows setting a custom function that will be called to output messages.
+         * 
+         * @param callable|null $callback - The callback function to set. If null, no callback is set.
+         */
         public function setOutputCallback (
             ?callable $callback
         ) : void {
